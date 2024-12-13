ecc96be40daf7557a00962d3bc76bb3c5e713a2e
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-13 11:52:57 -0300

    Iniciando o projeto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Clear();
+            Console.Write("Primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(valor1);
+        }
+    }
+}

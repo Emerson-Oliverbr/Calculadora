547a89db3b2c80aa53e550db6238ea7262bfc3ad
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-14 23:45:26 -0300

    Add: funcão soma ao metodo Soma

--- Program.cs
+++ Program.cs
@@ -5,19 +5,20 @@ namespace Calculadora
     internal class Program
     {
         static void Main(string[] args)
-        {            
+        {     
+            Soma();            
+        }
+
+        static void Soma() 
+        {
             Console.Clear();
             Console.Write("Primeiro valor: ");
             float valor1 = float.Parse(Console.ReadLine());
             Console.Write("Segundo Valor: ");
             float valor2 = float.Parse(Console.ReadLine());
             Console.WriteLine();
-
             float resultado = valor1 + valor2;
             Console.WriteLine($"O resultado da soma é: {resultado}");
-
-            Console.ReadKey();
-
         }
     }
 }

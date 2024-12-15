36233067b09d2e4e19f90e7dfc55137e5683140c
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-12-15 00:53:26 -0300

    Add: opção sair e finalizando a calculadora

--- obj/Debug/net9.0/Calculadora.AssemblyInfo.cs
+++ obj/Debug/net9.0/Calculadora.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+488f6af7175dd033e7c774f7b2288784fac532c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84356c7488f9cdf5f367d457e9b0ca95839c4bba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
